0a2bebb65ace34fa271bc1f6945966883ebee853
GeraldineSolis <solis.geraldine@tecsup.edu.pe>
2026-04-11 09:20:46 -0500

    tarea

--- semana04/mi-sitio-express/controllers/mainController.js
+++ semana04/mi-sitio-express/controllers/mainController.js
@@ -6,8 +6,48 @@ const about = (req, res) => {
     res.render("about", { title: "Acerca de" });
   };
 
+// "Base de datos" en memoria
+const messages = [];
+const listaJuegos = [];
+
+const contact = (req, res) => {
+    res.render("contact");
+}
+
+const saveContact = (req, res) => {
+  const { nombre, email, mensaje } = req.body;
+
+  // Guardar en memoria
+  messages.push({ nombre, email, mensaje });
+
+  // Redirigir a inicio para ver los mensajes
+  res.redirect('/admin');
+};
+
+const admin  = (req, res) => {
+  res.render("admin", { messages });
+};
+
+
+const juegos = (req, res) => {
+  res.render("juegos", { listaJuegos });
+}
+
+const saveJuego = (req, res) => {
+  const { titulo, plataforma, genero, calificacion, anio } = req.body;
+  listaJuegos.push({ titulo, plataforma, genero, calificacion, anio });
+  res.redirect('/juegos');
+};
+
+
 const mainController = {
-  home, about
+  home, 
+  about, 
+  contact,
+  saveContact,
+  admin,
+  juegos,
+  saveJuego
 };
 
 module.exports = mainController;
--- semana04/mi-sitio-express/routes/mainRoutes.js
+++ semana04/mi-sitio-express/routes/mainRoutes.js
@@ -5,5 +5,11 @@ const mainController = require("../controllers/mainController");
 // Definir rutas y asociarlas con controladores
 router.get("/", mainController.home);
 router.get("/about", mainController.about);
+router.get("/contact", mainController.contact);
+router.post("/contact", mainController.saveContact);
+router.get("/admin", mainController.admin);
+
+router.get("/juegos", mainController.juegos);
+router.post("/juegos", mainController.saveJuego);
 
 module.exports = router;
